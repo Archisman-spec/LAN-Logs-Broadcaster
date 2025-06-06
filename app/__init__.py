a314e4478e6d6b9fef6ddf710d7a431012a97682
Archisman Mandal <archiarchisman@gmail.com>
2025-06-07 00:55:05 +0530

    feat: testing of routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
 from app.routes.system_logs import system_logs_bp
+from app.routes.error_logs import error_logs_bp
+from app.routes.kernel_logs import kernel_logs_bp
+from app.routes.user_logs import user_logs_bp
+from app.routes.service_logs import service_logs_bp
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(system_logs_bp)
+    app.register_blueprint(error_logs_bp)
+    app.register_blueprint(kernel_logs_bp)
+    app.register_blueprint(user_logs_bp)
+    app.register_blueprint(service_logs_bp)
+
 
     return app
