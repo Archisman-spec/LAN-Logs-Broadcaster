171f278a47f44683a55f4bc5a68eec68dad326fc
Archisman Mandal <archiarchisman@gmail.com>
2025-06-09 01:30:15 +0530

    feat: testing

--- app/utils/journalctl_parser.py
+++ app/utils/journalctl_parser.py
@@ -1,4 +1,5 @@
 import subprocess
+import time
 
 def stream_journalctl(args=None):
     cmd = ['journalctl', '-f', '-n', '100', '--no-pager']
@@ -16,8 +17,25 @@ def stream_journalctl(args=None):
 
     def generator():
         try:
-            for line in process.stdout:
-                yield line
+            last_activity = time.time()
+            while True:
+                line = process.stdout.readline()
+                if line:
+                    yield line
+                    last_activity = time.time()
+                else:
+                    # If no new data for 10 seconds, close the connection
+                    if time.time() - last_activity > 10:
+                        break
+                    time.sleep(0.1)
+        except GeneratorExit:
+            process.terminate()
+            process.wait()
+            raise
+        except Exception as e:
+            process.terminate()
+            process.wait()
+            raise RuntimeError(f"Error reading journalctl output: {e}")
         finally:
             process.terminate()
             process.wait()
